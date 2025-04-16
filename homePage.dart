6c52e5e087e5ec6cf521b9619b32b73bb520d4d0
Anjali1883 <116424253+Anjali1883@users.noreply.github.com>
2025-04-16 13:46:58 +0530

    Create homePage.dart
    the initial screen shown to user

---
+++ homePage.dart
@@ -0,0 +1,33 @@
+// this is the home page which would be shown to the user as the app is used
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Voting App'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to the Voting App',
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
