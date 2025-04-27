9cc55a1cf6e6203cc41dcb887434d300263f0f57
Anjali1883 <116424253+Anjali1883@users.noreply.github.com>
2025-04-27 12:24:06 +0530

    Update and rename confirmationPage.dart to confirmation_page.dart

--- confirmation_page.dart
+++ confirmation_page.dart
@@ -1,9 +1,14 @@
 // user is asked for confirmation of their choice 
 
+// screens/confirmation_page.dart
+
 import 'package:flutter/material.dart';
 
 class ConfirmationPage extends StatelessWidget {
-  const ConfirmationPage({super.key});
+  final String candidateName;
+  final String eventId;
+
+  const ConfirmationPage({super.key, required this.candidateName, required this.eventId});
 
   @override
   Widget build(BuildContext context) {
@@ -16,14 +21,19 @@ class ConfirmationPage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text('Are you sure about your vote?'),
+            SizedBox(height: 10),
+            Text(
+              candidateName,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
+              onPressed: ()  {
                 Navigator.pushNamed(context, '/conclusion');
               },
               child: Text('Yes'),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 Navigator.pop(context);
